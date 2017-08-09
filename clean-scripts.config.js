1eaad82dafb05b82f565ec9620c87203ce302a24
york yao <yaoao12306@outlook.com>
2017-08-09 21:03:22 +0800

    add tslint fix script

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -26,6 +26,7 @@ module.exports = {
     'karma start spec/karma.config.js'
   ],
   fix: {
+    ts: `tslint --fix "*.ts"`,
     js: `standard --fix "**/*.config.js"`,
     less: `stylelint --fix "**/*.less"`
   }
