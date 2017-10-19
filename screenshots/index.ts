a50568ac5482b218ed54decf679fb1dbb1e5886b
york yao <yaoao12306@outlook.com>
2017-10-19 14:50:29 +0800

    improve test

--- screenshots/index.ts
+++ screenshots/index.ts
@@ -7,41 +7,46 @@ import * as puppeteer from "puppeteer";
     await page.goto(`http://localhost:8000`);
     await page.screenshot({ path: `screenshots/initial.png`, fullPage: true });
 
-    await page.focus("input");
-    await page.type("Task 1", { delay: 100 });
-    await page.press("Enter");
+    await (page.type as any)("input", "Task 1", { delay: 100 });
+    await (page.keyboard as any).press("Enter");
     await page.hover(".content");
     await page.waitFor(100);
     await page.screenshot({ path: `screenshots/new-task.png`, fullPage: true });
 
     await page.hover(".content");
     await page.waitFor(100);
-    await page.click("li button");
+    await page.click("li .on-it");
     await page.hover(".content");
     await page.waitFor(100);
     await page.screenshot({ path: `screenshots/on-it.png`, fullPage: true });
 
     await page.hover(".content");
     await page.waitFor(100);
-    await page.click("li button");
+    await page.click("li .done");
     await page.hover(".content");
     await page.waitFor(100);
     await page.screenshot({ path: `screenshots/done.png`, fullPage: true });
 
     await page.hover(".content");
     await page.waitFor(100);
-    await page.click("li button");
+    await page.click("li .reopen");
     await page.hover(".content");
     await page.waitFor(100);
     await page.screenshot({ path: `screenshots/reopen.png`, fullPage: true });
 
     await page.hover(".content");
     await page.waitFor(100);
-    await page.click("li:nth-child(2) button:nth-child(5)");
+    await page.click("li .close");
     await page.waitFor(100);
     await page.hover(".content");
     await page.waitFor(100);
     await page.screenshot({ path: `screenshots/close.png`, fullPage: true });
+
+    await page.hover(".content");
+    await page.waitFor(100);
+    await page.click("li .closed");
+    await page.waitFor(100);
+    await page.screenshot({ path: `screenshots/edit.png`, fullPage: true });
 
     browser.close();
 })();
