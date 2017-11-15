473e7dfbc466dc1090b8a962e906c29c174fe82e
york yao <yaoao12306@outlook.com>
2017-11-15 10:03:04 +0800

    update dependencies

--- screenshots/index.ts
+++ screenshots/index.ts
@@ -5,34 +5,34 @@ import * as puppeteer from "puppeteer";
     const page = await browser.newPage();
     await page.emulate({ viewport: { width: 1440, height: 900 }, userAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36" });
     await page.goto(`http://localhost:8000`);
-    await page.screenshot({ path: `screenshots/initial.png`, fullPage: true });
+    await page.screenshot({ path: `screenshots/initial.png` });
 
     await (page.type as any)("input", "Task 1", { delay: 100 });
     await (page.keyboard as any).press("Enter");
     await page.hover(".content");
     await page.waitFor(100);
-    await page.screenshot({ path: `screenshots/new-task.png`, fullPage: true });
+    await page.screenshot({ path: `screenshots/new-task.png` });
 
     await page.hover(".content");
     await page.waitFor(100);
     await page.click("li .on-it");
     await page.hover(".content");
     await page.waitFor(100);
-    await page.screenshot({ path: `screenshots/on-it.png`, fullPage: true });
+    await page.screenshot({ path: `screenshots/on-it.png` });
 
     await page.hover(".content");
     await page.waitFor(100);
     await page.click("li .done");
     await page.hover(".content");
     await page.waitFor(100);
-    await page.screenshot({ path: `screenshots/done.png`, fullPage: true });
+    await page.screenshot({ path: `screenshots/done.png` });
 
     await page.hover(".content");
     await page.waitFor(100);
     await page.click("li .reopen");
     await page.hover(".content");
     await page.waitFor(100);
-    await page.screenshot({ path: `screenshots/reopen.png`, fullPage: true });
+    await page.screenshot({ path: `screenshots/reopen.png` });
 
     await page.hover(".content");
     await page.waitFor(100);
@@ -40,19 +40,19 @@ import * as puppeteer from "puppeteer";
     await page.waitFor(100);
     await page.hover(".content");
     await page.waitFor(100);
-    await page.screenshot({ path: `screenshots/close.png`, fullPage: true });
+    await page.screenshot({ path: `screenshots/close.png` });
 
     await page.hover(".content");
     await page.waitFor(100);
     await page.click("li .closed");
     await page.waitFor(100);
-    await page.screenshot({ path: `screenshots/edit.png`, fullPage: true });
+    await page.screenshot({ path: `screenshots/edit.png` });
 
     await page.waitFor(100);
     await (page.keyboard as any).type(" changed");
     await (page.keyboard as any).press("Enter");
     await page.waitFor(100);
-    await page.screenshot({ path: `screenshots/edit-finished.png`, fullPage: true });
+    await page.screenshot({ path: `screenshots/edit-finished.png` });
 
     browser.close();
 })();
