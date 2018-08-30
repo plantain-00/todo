e42587291184c276032db345f1126aef7703080c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-30 23:26:26 +0000

    Deploy plantain-00/todo to github.com/plantain-00/todo.git:gh-pages

--- rev-static.config.js
+++ rev-static.config.js
@@ -22,6 +22,7 @@ module.exports = {
   customNewFileName: (filePath, fileString, md5String, baseName, extensionName) => baseName + '-' + md5String + extensionName,
   fileSize: 'file-size.json',
   context: {
-    prerender: fs.readFileSync('prerender/index.html')
+    prerender: fs.readFileSync('prerender/index.html'),
+    buildMoment: new Date().toString()
   }
 }
