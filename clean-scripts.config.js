c3378cb5d5d23be3154ca1d5176808227952c6f3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-09-04 00:52:46 +0000

    Deploy plantain-00/todo to github.com/plantain-00/todo.git:gh-pages

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -54,11 +54,9 @@ module.exports = {
   },
   watch: {
     template: `${templateCommand} --watch`,
-    src: `${tscCommand} --watch`,
     webpack: `${webpackCommand} --watch`,
     less: () => watch(['*.less'], [], () => executeScriptAsync(cssCommand)),
-    rev: `${revStaticCommand} --watch`,
-    sw: () => watch(['vendor.bundle-*.js', 'index.html', 'worker.bundle.js'], [], () => executeScriptAsync(swCommand))
+    rev: `${revStaticCommand} --watch`
   },
   screenshot: [
     new Service(`http-server -p 8000`),
