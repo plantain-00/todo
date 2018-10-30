e2e90d91247a01bf1de2e1835cdbabd7dcf0446b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-30 13:35:47 +0000

    Deploy plantain-00/todo to github.com/plantain-00/todo.git:gh-pages

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -41,7 +41,8 @@ module.exports = {
     less: `stylelint ${lessFiles}`,
     export: `no-unused-export ${tsFiles} ${lessFiles}`,
     commit: `commitlint --from=HEAD~1`,
-    markdown: `markdownlint README.md`
+    markdown: `markdownlint README.md`,
+    typeCoverage: 'type-coverage -p . --at-least 86'
   },
   test: [
     'tsc -p spec',
